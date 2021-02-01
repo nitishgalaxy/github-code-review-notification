886ed6105ceaa352170b9906274e3de7e5cffaf1
Nitish Saha <nitisaha@cisco.com>
2021-02-02 02:18:13 +0530

    Fixes

--- source/lib/data_store.js
+++ source/lib/data_store.js
@@ -1,18 +1,22 @@
 export default {
-	async get(key) {
-		const result = await browser.storage.local.get(key);
-		return result[key];
+
+	get(key) {
+		return new Promise(function(resolve, reject){
+			chrome.storage.local.get(key, function(result){
+				resolve(result[key]);
+			});
+		});
 	},
 
 	async set(key, value) {
-		return browser.storage.local.set({[key]: value});
+		return chrome.storage.local.set({[key]: value});
 	},
 
 	async remove(key) {
-		return browser.storage.local.remove(key);
+		return chrome.storage.local.remove(key);
 	},
 
 	async clear() {
-		return browser.storage.local.clear();
+		return chrome.storage.local.clear();
 	}
 };
---
+++ source/popup/popup.js
@@ -0,0 +1,40 @@
+import store from '../lib/data_store.js';
+
+$(document).ready(function () {
+
+function setSpanContent(element_id, text){
+    var span = document.getElementById(element_id);
+    while( span.firstChild ) {
+        span.removeChild( span.firstChild );
+    }
+    span.appendChild( document.createTextNode(text));
+}
+
+async function loadGithubUrls(){
+    const github_url_1 = await store.get('github_url_1');
+    const github_url_2 = await store.get('github_url_2');
+    console.log("github_url_1 :", github_url_1, "github_url_2 :", github_url_2);
+    setSpanContent("github_url_1", github_url_1);
+    setSpanContent("github_url_1", github_url_2);
+
+}
+
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+    for (var key in changes) {
+      var storageChange = changes[key];
+      
+      console.log('Storage key "%s" in namespace "%s" changed. ' +
+                  'Old value was "%s", new value is "%s".',
+                  key,
+                  namespace,
+                  storageChange.oldValue,
+                  storageChange.newValue);
+    
+     if(key == 'github_url_1' || key == 'github_url_2')
+        loadGithubUrls();
+     }
+  });
+
+loadGithubUrls()
+});
